b370073ef4eb984282df173135db9ba5dff1f88a
Marwa Mahmoud <Marwa@Marwa--Mariam-MacBook-Pro.local>
2017-08-31 13:39:09 -0700

    scope safe constructors plus adding an array as front end data

--- BasicCard.js
+++ BasicCard.js
@@ -2,35 +2,74 @@
 module.exports = BasicCard;
 
 var inquirer = require('inquirer');
+var fs = require('fs');
+var frontEndArray = [    
+    {
+        front: "Who is the first president of the United States?",
+        back: "George Washington"
+    },
+    {
+        front:  "What is the tallest mountain in the world?",
+        back: "Mountain Everest"
+    }
+];
 
-var BasicCard = function(front, back){
+var count = 0;
 
-    this.front = front;
-    this.back = back;
+function BasicCard (front, back){
+    
+        if(this instanceof BasicCard){
+    
+            this.front = front;
+            this.back = back;
+        }
+    
+        else return new BasicCard(front, back);
+            
+    }
 
+
+
+
+function playFlashCards(){
+ 
+    if(count < frontEndArray.length){
+
+        var basicCard = BasicCard(frontEndArray[count].front, frontEndArray[count].back);
+
+        inquirer.prompt([
+            
+                    {
+                        type: "input",
+                        name: "answer",
+                        message: "Front: "+basicCard.front+"\n"+"Back: ",
+                     }
+            
+                ]).then(function(answers){
+        
+                    if(answers.answer.toLowerCase() === basicCard.back.toLowerCase()){
+                        console.log("Correct Answer!");
+                    }
+        
+                    else{
+        
+                        console.log("Wrong Answer :(");
+                        console.log("The correct answer is: " + basicCard.back);
+                    }
+
+                    count++;
+                    playFlashCards();
+                       
+                    });
+
+    }
+
+    else{
+        console.log("No more flash cards!");
+    }
 }
+playFlashCards();
 
-var basicCard1 = new BasicCard("What is your name?", "Marwa");
 
-inquirer.prompt([
-    
-            {
-                type: "input",
-                name: "answer",
-                message: basicCard1.front,
-             }
-    
-        ]).then(function(answers){
-
-            if(answers.answer === basicCard1.back){
-                console.log("Correct Answer!");
-            }
-
-            else{
-
-                console.log("Wrong Answer :(");
-                console.log("The correct answer is: " + basicCard1.back);
-            }
-               
-            });
+          
     
--- ClozeCard.js
+++ ClozeCard.js
@@ -1,32 +1,90 @@
 module.exports = ClozeCard;
 
 var inquirer = require('inquirer');
-//var BasicCard = require('./BasicCard.js');
+var frontEndArray = [    
+    {
+        fullText: "George Washington is the first president of the United States",
+        cloze: "George Washington"
+    },
+    {
+       fullText: "The tallest mountain in the world is Mountain Everest",
+       cloze: "Mountain Everest"
+    }
+];
 
-var ClozeCard = function(text, cloze){
-    this.cloze = cloze;
-    this.fullText = text;
+var count = 0;
 
-    if(text.includes(cloze)){
-        this.partial = text.replace(cloze, " ... ");
+function ClozeCard (text, cloze){
+
+    if(this instanceof ClozeCard){
+
+        this.cloze = cloze;
+        this.fullText = text;
+        this.partial = 'No questions to be asked';
+        this.hasIt = text.includes(cloze);
+    
+        if(text.includes(cloze)){
+            this.partial = text.replace(cloze, " ... ");
+        }
+
+    }
+    else{
+
+        return new ClozeCard(text, cloze);
+    }
+
+
+}
+
+function playFlashCards(){
+
+    if(count < frontEndArray.length){
+
+        var clozeCard = new ClozeCard(frontEndArray[count].fullText, frontEndArray[count].cloze);
+
+        if(clozeCard.hasIt === false){
+            
+                console.log('The cloze does not appear in the text');
+            
+            }
+            
+            else{
+            
+            
+                inquirer.prompt([
+                    
+                            {
+                                type: "input",
+                                name: "answer",
+                                message: clozeCard.partial+'\n',
+                             }
+                    
+                        ]).then(function(answers){
+                
+                            if(answers.answer.toLowerCase() === clozeCard.cloze.toLowerCase()){
+                                console.log("Correct Answer!");
+                            }
+                
+                            else{
+                
+                                console.log("Wrong Answer :(");
+                                console.log("The correct answer is: " + clozeCard.fullText);
+                            }
+                             
+                            count++;
+                            playFlashCards();
+                            });
+            
+            }            
+
     }
 
     else{
-        console.log("The cloze you entered doesn't exist!");
-    }
-
-
-   
-    this.hasIt = text.includes(cloze);
-   
-
-    this.createParialText = function(){
-
-    console.log(this.partial);
-    console.log(this.hasIt);
+        console.log("No more flash cards!");
     }
 }
 
+playFlashCards();
 
-var clozeCard1 = new ClozeCard("Marwa is my name", "marwa");
-clozeCard1.createParialText();
+
+
